1144c7faf6037778273f20c3c53e8b85efbfd42a
Kevin Chi <kevinchi118@gmail.com>
2018-08-01 16:03:47 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,19 @@
 class PigLatinizer
 
-  def initialize(paragraph)
+  def self.translate(string)
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
+
+    if vowels.include?(string[0])
+      string + 'ay'
+    elsif consonants.include?(string[0]) && consonants.include?(string[1])
+      string[2..-1] + string[0..1] + 'ay'
+    elsif consonants.include?(string[0])
+      string[1..-1] + string[0] + 'ay'
+    else
+      string
+    end 
+  end
 
 end
